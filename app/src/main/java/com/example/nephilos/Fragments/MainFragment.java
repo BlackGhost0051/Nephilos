1da2e8ca474db8f44db5d6cb9ee9977b8b7a85be
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2024-06-23 12:24:42 +0200

    Add gitHub link

--- app/src/main/java/com/example/nephilos/Fragments/MainFragment.java
+++ app/src/main/java/com/example/nephilos/Fragments/MainFragment.java
@@ -1,5 +1,7 @@
 package com.example.nephilos.Fragments;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,15 +9,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.nephilos.R;
 
 
 public class MainFragment extends Fragment {
 
+    Button gitHubBtn;
 
     public MainFragment() {
-        // Required empty public constructor
     }
 
 
@@ -28,6 +31,18 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        gitHubBtn = view.findViewById(R.id.gitHubBtn);
+
+        gitHubBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(getString(R.string.gitHubUrl)));
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
