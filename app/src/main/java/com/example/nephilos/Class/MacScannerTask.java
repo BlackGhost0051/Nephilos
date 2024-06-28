1ada298e1c26c1edac36c78fd4078a573433d4f4
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2024-06-28 20:26:48 +0200

    Mod MacScannerTask

--- app/src/main/java/com/example/nephilos/Class/MacScannerTask.java
+++ app/src/main/java/com/example/nephilos/Class/MacScannerTask.java
@@ -1,4 +1,30 @@
 package com.example.nephilos.Class;
 
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class MacScannerTask {
+    public String getArpTable(){
+        try {
+            Process p = Runtime.getRuntime().exec("su -c ip n show");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            StringBuilder output = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line).append("\n");
+            }
+            reader.close();
+            p.waitFor();
+
+            String result = output.toString();
+            p.destroy();
+            return result;
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
