cb2669f3d147002977520fb0787c3cefd2158761
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2024-06-29 08:59:49 +0200

    Add pingIp

--- app/src/main/java/com/example/nephilos/Class/MacScannerTask.java
+++ app/src/main/java/com/example/nephilos/Class/MacScannerTask.java
@@ -5,6 +5,7 @@ import android.widget.Toast;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.InetAddress;
 
 public class MacScannerTask {
     public String getArpTable(){
@@ -29,5 +30,15 @@ public class MacScannerTask {
     }
     public boolean pingAllIp(String host){
         return false;
+    }
+
+    public boolean pingIp(String ip){
+        try{
+            InetAddress inetAddress = InetAddress.getByName(ip);
+            return inetAddress.isReachable(500);
+        } catch (IOException e){
+            e.printStackTrace();
+            return false;
+        }
     }
 }
