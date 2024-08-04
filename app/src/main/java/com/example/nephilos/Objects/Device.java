c95089821a13f2fea52da483f938135dd752f8f9
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2024-08-04 16:27:10 +0200

    Add Device Object

---
+++ app/src/main/java/com/example/nephilos/Objects/Device.java
@@ -0,0 +1,22 @@
+package com.example.nephilos.Objects;
+
+public class Device {
+    private String ip;
+    private String mac;
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getMac() {
+        return mac;
+    }
+
+    public void setMac(String mac) {
+        this.mac = mac;
+    }
+}
