eaa6191b4b72c7f6385ff79f9fd403eae902c67a
Black Ghost <147267828+BlackGhost0051@users.noreply.github.com>
2024-08-15 18:04:48 +0200

    Add and Test LinearLayout

--- app/src/main/java/com/blackghost/nephilos/Fragments/RequestFragment.java
+++ app/src/main/java/com/blackghost/nephilos/Fragments/RequestFragment.java
@@ -7,6 +7,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.AdapterView;
 
 import com.blackghost.nephilos.R;
 
@@ -30,7 +33,32 @@ public class RequestFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_request, container, false);
 
+        Spinner requestTypeSpinner = view.findViewById(R.id.request_type_spinner);
+        LinearLayout getLayout = view.findViewById(R.id.get_layout);
+        LinearLayout postLayout = view.findViewById(R.id.post_layout);
 
+        requestTypeSpinner.setSelection(0);
+        getLayout.setVisibility(View.VISIBLE);
+        postLayout.setVisibility(View.GONE);
+
+        requestTypeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedRequest = parent.getItemAtPosition(position).toString();
+
+                if (selectedRequest.equals("GET")) {
+                    getLayout.setVisibility(View.VISIBLE);
+                    postLayout.setVisibility(View.GONE);
+                } else if (selectedRequest.equals("POST")) {
+                    getLayout.setVisibility(View.GONE);
+                    postLayout.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
 
         return view;
     }
