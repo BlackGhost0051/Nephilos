ff434af9f7fe5f7b1a71972463fd04208df4a2b9
Black Ghost <147267828+BlackGhost0051@users.noreply.github.com>
2024-08-16 12:57:46 +0200

    Add libs

--- app/src/main/cpp/send_icmp_to_all.c
+++ app/src/main/cpp/send_icmp_to_all.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/ip_icmp.h>
+#include <netinet/in.h>
 
 int main(){
     return 0;
