c590381a2bb81f06ef0c3d034cbe76242d0aa140
Black Ghost <147267828+BlackGhost0051@users.noreply.github.com>
2024-08-18 11:21:05 +0200

    Mod styles

--- app/src/main/java/com/blackghost/nephilos/Fragments/SettingsFragment.java
+++ app/src/main/java/com/blackghost/nephilos/Fragments/SettingsFragment.java
@@ -10,5 +10,6 @@ public class SettingsFragment  extends PreferenceFragmentCompat{
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.preferences, rootKey);
+        getActivity().setTheme(R.style.preferences_style);
     }
 }
