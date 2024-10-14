97b7c6b3e642615831a3d7ba1b0b91298902fe2f
Black Ghost <147267828+BlackGhost0051@users.noreply.github.com>
2024-10-14 11:33:26 +0200

    Mod RequestManager

--- app/src/main/java/com/blackghost/nephilos/Managers/RequestManager.java
+++ app/src/main/java/com/blackghost/nephilos/Managers/RequestManager.java
@@ -1,4 +1,15 @@
 package com.blackghost.nephilos.Managers;
 
 public class RequestManager {
+    public RequestManager(){
+
+    }
+
+    public void send_GET(){
+
+    }
+
+    public void send_POST(){
+
+    }
 }
