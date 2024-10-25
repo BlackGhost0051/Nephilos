e0b8867dc4f64dee0fb949c56f76efc50e70c41f
Black Ghost <147267828+BlackGhost0051@users.noreply.github.com>
2024-10-25 19:35:13 +0200

    Mod

--- app/src/main/java/com/blackghost/nephilos/Class/PortScannerTask.java
+++ app/src/main/java/com/blackghost/nephilos/Class/PortScannerTask.java
@@ -2,14 +2,12 @@ package com.blackghost.nephilos.Class;
 
 public class PortScannerTask {
     String ip;
-    String port;
 
-    public PortScannerTask(String ip, String port){
+    public PortScannerTask(String ip){
         this.ip = ip;
-        this.port = port;
     }
 
-    void scan_tcp_port(){
+    public void scan_tcp_ports(){
 
     }
 }
