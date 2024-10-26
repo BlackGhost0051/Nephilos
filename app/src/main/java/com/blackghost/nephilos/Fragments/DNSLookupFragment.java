47bb5668d61345cae78b56cb87984fa51924d0b7
Black Ghost <147267828+BlackGhost0051@users.noreply.github.com>
2024-10-26 20:18:18 +0200

    Mod fragment

--- app/src/main/java/com/blackghost/nephilos/Fragments/DNSLookupFragment.java
+++ app/src/main/java/com/blackghost/nephilos/Fragments/DNSLookupFragment.java
@@ -7,11 +7,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.blackghost.nephilos.R;
 
 
 public class DNSLookupFragment extends Fragment {
+
+    private EditText dnsInput;
+    private TextView dnsInfo;
 
     public DNSLookupFragment() {
         // Required empty public constructor
@@ -27,7 +33,11 @@ public class DNSLookupFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dns_lookup, container, false);
+        View view= inflater.inflate(R.layout.fragment_dns_lookup, container, false);
+
+        dnsInfo = view.findViewById(R.id.dns_info);
+        dnsInput = view.findViewById(R.id.dns_input_ip);
+
+        return view;
     }
 }
