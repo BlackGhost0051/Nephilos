aec8d150b32d14836a4f1f96f6d4d71a927fde76
Black Ghost <147267828+BlackGhost0051@users.noreply.github.com>
2024-10-29 13:31:33 +0100

    Add ProgressBar

--- app/src/main/java/com/blackghost/nephilos/Fragments/ArpSpoofFragment.java
+++ app/src/main/java/com/blackghost/nephilos/Fragments/ArpSpoofFragment.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.blackghost.nephilos.Managers.LibraryManager;
@@ -28,6 +29,8 @@ public class ArpSpoofFragment extends Fragment {
     // Add icmp_send host 1 -> 255
 
     public ArpSpoofFragment() {}
+
+    private ProgressBar progressBar;
 
     private String nativeLibraryDir;
     private TextView info_view;
@@ -54,6 +57,8 @@ public class ArpSpoofFragment extends Fragment {
 
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
 
+        progressBar = view.findViewById(R.id.progress_bar);
+
         start_spoof_btn = view.findViewById(R.id.start_spoof_btn);
         info_view = view.findViewById(R.id.info_view);
 
@@ -67,6 +72,7 @@ public class ArpSpoofFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 String interfaceName = sharedPreferences.getString("interface_name", "wlan0");
+                progressBar.setVisibility(View.VISIBLE);
                 start_arp_spoof(interfaceName, String.valueOf(source_ip_input.getText()), String.valueOf(source_mac_input.getText()),String.valueOf(target_ip_input.getText()),String.valueOf(target_mac_input.getText()));
             }
         });
