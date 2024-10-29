214b491380191215b053e8b4eaac4ed98b5cc115
Black Ghost <147267828+BlackGhost0051@users.noreply.github.com>
2024-10-29 17:24:26 +0100

    Add DNSManager

---
+++ app/src/main/java/com/blackghost/nephilos/Managers/DNSManager.java
@@ -0,0 +1,22 @@
+package com.blackghost.nephilos.Managers;
+
+public class DNSManager {
+    private String stringUrl;
+
+
+    public DNSManager(){
+
+    }
+
+    public String getStringUrl() {
+        return stringUrl;
+    }
+
+    public void setStringUrl(String stringUrl) {
+        this.stringUrl = stringUrl;
+    }
+
+
+    // get all info from dns
+    // get all address
+}
